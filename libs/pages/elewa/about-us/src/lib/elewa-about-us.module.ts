5d7eea28eda3943c0578e3be69a19cc02d8ac53e
michael-mbugua <kamirimichael369@gmail.com>
2023-02-09 09:52:46 +0300

    create declarations

--- libs/pages/elewa/about-us/src/lib/elewa-about-us.module.ts
+++ libs/pages/elewa/about-us/src/lib/elewa-about-us.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AboutCarouselComponent } from './about-carousel/about-carousel.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [AboutCarouselComponent],
 })
 export class AboutUsModule {}
